6d00924157b63ae053822bc01f66e32deb810da0
RebecaBasca <73829339+RebecaBasca@users.noreply.github.com>
2021-08-26 00:22:25 +0300

    inscriere.js

--- portal/js/each/inscriere.js
+++ portal/js/each/inscriere.js
@@ -2,27 +2,35 @@ $("form").submit(function (e){
     e.preventDefault()
 
     //database prepare
-    console.log("testtttttttttttt")
     let formData = {
-        blabla:"blabla"
+        user_id:getUserId(),
+        afilierea:document.querySelector("input[id='afiliere']").value,
+        statut:document.querySelector("input[id='statut']").value,
+        titlu:document.querySelector("input[id='titlu']").value,
+        sectiune_colocviu:document.querySelector("input[id='sectiune']").value,
+        rezumat: getTextValue($("#rezumat")),
+        cuv_cheie: getTextValue($("#cuvintecheie")),
+        bibliografie: getTextValue($("#bibliografie")),
+        nota_bibliografica: getTextValue($("#notabiobibliografica"))
+
     }
 
     // database send
-    // database.ref("cieft").once('value')
-    //     .then((snapshot)=>{
-    //         let users = snapshot.val()
-    //         if(users === null){
-    //             //primul, users = [obj]
-    //         }
-    //         else{
-    //             //push in array, users = [..., obj]
-    //         }
-    //
-    //         //saving
-    //         let updates = {}
-    //         updates['/cieft'] = users
-    //         database.ref().update(updates)
-    //     })
+    database.ref("cieft").once('value')
+        .then((snapshot)=>{
+            let users = snapshot.val()
+            if(users === null){
+                 users = [formData]
+            }
+            else{
+                users.push(formData)
+            }
+
+            //saving
+            let updates = {}
+            updates['/cieft'] = users
+            database.ref().update(updates)
+        })
 
 })
 
