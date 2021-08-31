e0bfa6fc1b2665b5db0c97de7d1a24ae9e8f4cd2
RebecaBasca <73829339+RebecaBasca@users.noreply.github.com>
2021-08-31 21:33:52 +0300

    profil

--- portal/js/each/profile.js
+++ portal/js/each/profile.js
@@ -170,9 +170,8 @@ database.ref("users").once('value')
             // ARTICOLE CE TREBUIE ATRIBUITE EVALUATORILOR-
 
 
-            //if(RIGHTS[my_rank][`${pub}-articole-assign-panel`] === true){
+            if(RIGHTS[my_rank][`${pub}-articole-assign-panel`] === true){
 
-            if (true){
                 /////////////////////////////////////////////////////////////////////////////////////////////
                 let evaluatori = []
                 for(const [key,user] of Object.entries(USERS)){
@@ -192,19 +191,26 @@ database.ref("users").once('value')
                 </div>
                 `)
 
-                database.ref(PUB+"/propuneri").once("value")
-                    .then((snap)=>{
-                        let propuneri= snap.val()
-                        for(const [key,propunere] of Object.entries(propuneri)){
-                            document.getElementById("trebuie_atribuite_lista").insertAdjacentHTML("beforeend", `
-                       <div class="assign-article-container">
-                    <div>
-                        <div class="assign-article-info-container">
-                            <p>Nume articol &emsp; <svg class="redirect-svg" viewBox="0 0 25 25" fill="none"><path d="M23.8636 0.0303345H14.7727C14.1451 0.0303345 13.6363 0.527406 13.6363 1.14054C13.6363 1.75367 14.1451 2.25075 14.7727 2.25075H21.1202L9.42378 13.6779C8.97999 14.1114 8.97999 14.8143 9.42378 15.2479C9.64561 15.4646 9.93643 15.573 10.2273 15.573C10.5181 15.573 10.8089 15.4647 11.0308 15.2478L22.7273 3.82077V10.0221C22.7273 10.6352 23.2361 11.1323 23.8637 11.1323C24.4913 11.1323 25.0001 10.6352 25.0001 10.0221V1.14054C25 0.527406 24.4912 0.0303345 23.8636 0.0303345Z" fill="black"/><path d="M19.3182 11.1322C18.6906 11.1322 18.1818 11.6293 18.1818 12.2424V22.2342H2.27271V6.69143H12.5C13.1276 6.69143 13.6364 6.19436 13.6364 5.58123C13.6364 4.96809 13.1276 4.47107 12.5 4.47107H1.13638C0.508789 4.47107 0 4.96814 0 5.58128V23.3444C0 23.9575 0.508789 24.4545 1.13638 24.4545H19.3182C19.9458 24.4545 20.4546 23.9575 20.4546 23.3443V12.2424C20.4545 11.6293 19.9458 11.1322 19.3182 11.1322Z" fill="black"/></svg></p>
-                            <p>Autor: Elton John</p>
-                            <p>Propus in 13 Aprilie 2021, 21:21</p>
-                            <p>btn</p>
-                        </div>
+
+                database.ref(PUB + "/propuneri").once("value")
+                    .then((snap) => {
+                        const propuneri = snap.val()
+
+                        Contributions.forEach((contrib) => {
+                            const contributie = propuneri[contrib]
+
+                            if( contributie.stadiu === 1)
+                            {
+
+                                document.getElementById("trebuie_atribuite_lista").insertAdjacentHTML("beforeend", `
+                             <div class="assign-article-container">
+                            <div>
+                            <div class="assign-article-info-container">
+                                <p>${contributie.titlu} &emsp; <svg class="redirect-svg" viewBox="0 0 25 25" fill="none"><path d="M23.8636 0.0303345H14.7727C14.1451 0.0303345 13.6363 0.527406 13.6363 1.14054C13.6363 1.75367 14.1451 2.25075 14.7727 2.25075H21.1202L9.42378 13.6779C8.97999 14.1114 8.97999 14.8143 9.42378 15.2479C9.64561 15.4646 9.93643 15.573 10.2273 15.573C10.5181 15.573 10.8089 15.4647 11.0308 15.2478L22.7273 3.82077V10.0221C22.7273 10.6352 23.2361 11.1323 23.8637 11.1323C24.4913 11.1323 25.0001 10.6352 25.0001 10.0221V1.14054C25 0.527406 24.4912 0.0303345 23.8636 0.0303345Z" fill="black"/><path d="M19.3182 11.1322C18.6906 11.1322 18.1818 11.6293 18.1818 12.2424V22.2342H2.27271V6.69143H12.5C13.1276 6.69143 13.6364 6.19436 13.6364 5.58123C13.6364 4.96809 13.1276 4.47107 12.5 4.47107H1.13638C0.508789 4.47107 0 4.96814 0 5.58128V23.3444C0 23.9575 0.508789 24.4545 1.13638 24.4545H19.3182C19.9458 24.4545 20.4546 23.9575 20.4546 23.3443V12.2424C20.4545 11.6293 19.9458 11.1322 19.3182 11.1322Z" fill="black"/></svg></p>
+                                <p>Autor: ${contributie.autor}</p>
+                                <p>Propus in ${contributie.data}</p>
+                                <p>btn</p>
+                             </div>
                         <div class="assign-article-evals-container">
                             <p>Selecteaza evaluatori</p>
                             <div>
@@ -224,18 +230,20 @@ database.ref("users").once('value')
 
                 </div>
                         `)
-                            evaluatori.forEach((evaluator) => {
-                                document.getElementById(`${propunere.id}-assign-evaluatori-1`).insertAdjacentHTML("beforeend",`
+                                evaluatori.forEach((evaluator) => {
+                                    document.getElementById(`${propunere.id}-assign-evaluatori-1`).insertAdjacentHTML("beforeend", `
                                 <option value="${evaluator[1]}">${evaluator[0]}</option>
                                 `)
-                                document.getElementById(`${propunere.id}-assign-evaluatori-2`).insertAdjacentHTML("beforeend",`
+                                    document.getElementById(`${propunere.id}-assign-evaluatori-2`).insertAdjacentHTML("beforeend", `
                                 <option value="${evaluator[1]}">${evaluator[0]}</option>
                                 `)
-                            })
+                                })
 
-                        }
+                            }
 
+                        })
                     })
+
 
 
             }
